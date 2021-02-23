811038a06f472ec9576996e3ce809e4089228831
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-02-23 17:30:46 -0300

    2°Dia: Desvendando o Next.js | Parte 2

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,13 +2,19 @@ import Head from 'next/head'
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from "../components/ExperienceBar";
 import {Profile} from '../components/Profile'
-import styles from '../styles/components/Home.module.css'
-import { Countdown } from './Countdown';
+import { Countdown } from '../components/Countdown';
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>      
+    <div className={styles.container}>  
+      <Head>
+        <title>Início | move.it</title>
+      </Head>    
+
       <ExperienceBar />
+      
       <section>
         <div>
           <Profile />
